6f3e0b41b3d5bf4847d24120d7d1fed39f2691f1
RunaNam <nmj5012@naver.com>
2021-12-05 20:09:53 +0900

    refactor(PaymentMethodVadlidator): 에러 메시지 수정

--- src/main/java/validator/PaymentMethodValidator.java
+++ src/main/java/validator/PaymentMethodValidator.java
@@ -6,7 +6,7 @@ public class PaymentMethodValidator {
 
 	public static void isRightNumber(Integer featureNumber) {
 		if (featureNumber != 1 && featureNumber != 2) {
-			throw new IllegalArgumentException(ERROR_FEATURE_NUM_WRONG_NUM);
+			throw new IllegalArgumentException(ERROR_WRONG_PAYMENT_METHOD);
 		}
 	}
 
